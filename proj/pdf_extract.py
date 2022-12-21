52c9d194cc2c3b24e3f5ca75e2fa4c4bc5c76ac1
seonu <2013101066sw@gmail.com>
2022-12-21 20:53:54 +0900

    Update pdf_extract.py

--- proj/pdf_extract.py
+++ proj/pdf_extract.py
@@ -1,13 +1,13 @@
 import tabula
 import os
 import shutil
+from datetime import date
 
 class TaxInfoAnalyzer() :
 
     def __init__(self,file_path:str) :
 
         dfs = tabula.read_pdf(file_path,pages='all')
-
         dfs[1].columns = dfs[1].loc[0,:]
         dfs[1] = dfs[1].drop(0)
         dfs[1] = dfs[1].iloc[:-1,:].set_index('세목명')
@@ -16,17 +16,40 @@ class TaxInfoAnalyzer() :
         self.file_path = file_path
         self.company_name = dfs[0].values[2][1]
         self.taxes = taxes[taxes>0]
+        self.multiple_taxes = False
+        if len(self.taxes) > 1 :
+            self.multiple_taxes = True
 
-    def check_taxlist(self,target_dir) :
+def initialize_working_directory(file_dir) :
+    date_today = date.today().strftime("%Y%m%d")
+    file_dir = os.path.join(file_dir,date_today)
 
-        if len(self.taxes)>1 :
-            file_dir,file_name = os.path.split(self.file_path)
-            target_path = os.path.join(target_dir,file_name)
-            shutil.move(self.file_path,target_path)
-        else :
-            # TODO : check if directory exists
-            # if exists, move file
-            # else, make directory and move
-            pass
+    # initialize today's working directory
+    if os.path.exists(file_dir) :
+        shutil.rmtree(file_dir)
+    os.makedirs(file_dir)
 
-            
+    return file_dir
+
+
+def move_file(tia:TaxInfoAnalyzer,working_dir) :
+
+    file_path,file_name = os.path.split(tia.file_path)
+
+    # name target dir
+    if tia.multiple_taxes :
+        target_dir = os.path.join(working_dir,"CHECK_PLEASE")
+    else :
+        target_dir = os.path.join(working_dir,tia.company_name)
+    
+    if not os.path.exists(target_dir) :
+        os.makedirs(target_dir)
+
+    target_path = os.path.join(target_dir,file_name)
+
+    shutil.copyfile(tia.file_path,target_path)
+    
+
+    
+    
+
