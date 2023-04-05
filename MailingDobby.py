d77d89591f4c77a3087799bcd427cf0e9c68f299
seonu <2013101066sw@gmail.com>
2023-04-05 14:26:16 +0900

    add button and split process

--- MailingDobby.py
+++ MailingDobby.py
@@ -6,8 +6,7 @@ from tkinter.messagebox import askyesno, showinfo
 from configparser import ConfigParser
 import os 
 import platform
-
-from automate import run
+from automate import analyze_pdf,send_mail
 
 if platform.system()=='Darwin' :
     root_dir = "/User/seonwoolim/Desktop/"
@@ -16,19 +15,21 @@ elif platform.system()=='Windows' :
 else :
     raise NotImplementedError
 
+
 class ConfigGUI:
     def __init__(self, master=None):
         # build ui
-        self.mainframe = ttk.Frame(master)
-        self.mainframe.configure(height=300, width=500)
+        self.frame = ttk.Frame(master)
+        self.frame.configure(height=500, padding=30, width=600)
         self.pdf_dir = tk.StringVar()
         self.contacts_path = tk.StringVar()
         self.mail_address_text = ""
         self.password_text = ""
         self.mail_title_text = ""
         self.mail_body_text = ""
+        self.wd = None
 
-        # Check path when initializing
+       # Check path when initializing
         config_dir = os.path.join(root_dir,".mailing_dobby")
         self.config_path = os.path.join(config_dir,"info.cfg")
         if not os.path.exists(config_dir) :
@@ -45,88 +46,97 @@ class ConfigGUI:
 
         self.pdf_dir_part()
         self.contacts_path_part()
+        self.login_part()
         self.mail_title_part()
         self.mail_body_part()
-        self.login_part()
-        self.submit_part()
-        self.mainframe.place(anchor="nw", x=0, y=0)
+        self.analyze_button()
+        self.send_mail_button()
 
         # Main widget
-        self.mainwindow = self.mainframe
+        self.frame.place(anchor="nw", x=0, y=0)
+        self.mainwindow = self.frame
+
 
     def pdf_dir_part(self) :
-        ## label
-        self.pdf_path_label = ttk.Label(self.mainframe)
-        self.pdf_path_label.configure(text='PDF 파일위치')
-        self.pdf_path_label.place(anchor="nw", relx=0.1, rely=0.01, x=0, y=0)
-        ## Entry
-        self.pdf_file_path_entry = ttk.Entry(self.mainframe,textvariable=self.pdf_dir)
-        self.pdf_file_path_entry.place(anchor="nw", relx=0.33, rely=0.01, x=0, y=0)
-        ## Button
-        self.browse_pdf_path = ttk.Button(self.mainframe,command=self.get_pdf_dir)
-        self.browse_pdf_path.configure(text='browse')
-        self.browse_pdf_path.place(anchor="nw",relheight=0.08,relwidth=0.13,relx=0.74,rely=0.01,x=0,y=0)
+        # label
+        self.label1 = ttk.Label(self.frame)
+        self.label1.configure(text='PDF파일위치')
+        self.label1.place(anchor="nw", relx=0.0, rely=0.01, x=0, y=0)
+        # entry
+        self.entry1 = ttk.Entry(self.frame,textvariable=self.pdf_dir)
+        self.entry1.place(anchor="nw", relwidth=0.5, relx=0.30, rely=0.01, x=0, y=0)
+        # button
+        self.button1 = tk.Button(self.frame,command=self.get_pdf_dir)
+        self.button1.configure(text='browse')
+        self.button1.place(anchor="nw", relx=0.85, x=0, y=0)
 
     def contacts_path_part(self) :
-        ## label
-        self.contacts_path_label = ttk.Label(self.mainframe)
-        self.contacts_path_label.configure(text='contacts파일선택')
-        self.contacts_path_label.place(anchor="nw", relx=0.1, rely=0.12, x=0, y=0)
-
+        # label
+        self.label2 = ttk.Label(self.frame)
+        self.label2.configure(text='contacts파일선택')
+        self.label2.place(anchor="nw", relx=0.0, rely=0.09, x=0, y=0)
         # entry
-        self.contacts_entry = ttk.Entry(self.mainframe,textvariable=self.contacts_path)
-        self.contacts_entry.place(anchor="nw", relx=0.33, rely=0.12, x=0, y=0)
-
+        self.entry2 = ttk.Entry(self.frame,textvariable=self.contacts_path)
+        self.entry2.place(anchor="nw", relwidth=0.5, relx=0.30, rely=0.09, x=0, y=0)
         # button
-        self.browse_contacts = ttk.Button(self.mainframe,command=self.get_contacts_path)
-        self.browse_contacts.configure(takefocus=False, text='browse')
-        self.browse_contacts.place(anchor="nw",relheight=0.08,relwidth=0.13,relx=0.74,rely=0.12,x=0,y=0)
+        self.button2 = tk.Button(self.frame,self.get_contacts_path)
+        self.button2.configure(text='browse')
+        self.button2.place(anchor="nw", relx=0.85, rely=0.09, x=0, y=0)
 
-    def mail_title_part(self):
-        ## label
-        self.mail_title_label = ttk.Label(self.mainframe)
-        self.mail_title_label.configure(text='메일제목')
-        self.mail_title_label.place(anchor="nw", relx=0.1, rely=0.31, x=0, y=0)
-        ## entry
-        self.mail_title_entry = ttk.Entry(self.mainframe)
-        self.mail_title_entry.insert(tk.END,self.mail_title_text)
-        self.mail_title_entry.configure(justify="left")
-        self.mail_title_entry.place(anchor="nw",relwidth=0.71,relx=0.2,rely=0.31,x=0,y=0)
-
-    def mail_body_part(self):
-        ## label
-        self.mail_body_label = ttk.Label(self.mainframe)
-        self.mail_body_label.configure(text='메일내용')
-        self.mail_body_label.place(anchor="nw", relx=0.1, rely=0.44, x=0, y=0)
-        ## entry
-        self.mail_body_entry = tk.Text(self.mainframe)
-        self.mail_body_entry.insert(tk.END,self.mail_body_text)
-        self.mail_body_entry.configure(height=10, width=50)
-        self.mail_body_entry.place(anchor="nw",relwidth=0.71,relheight=0.4,relx=0.2,rely=0.42,x=0,y=0)
 
     def login_part(self) :
+
         # Address part
-        self.mail_address_entry = ttk.Entry(self.mainframe)
-        self.mail_address_entry.insert(tk.END,self.mail_address_text)
-        self.mail_address_entry.place(anchor="nw",relwidth=0.28,relx=0.17,rely=0.21,x=0,y=0)
-        self.mail_address_label = ttk.Label(self.mainframe)
-        self.mail_address_label.configure(text='메일주소')
-        self.mail_address_label.place(anchor="nw", relx=0.05, rely=0.21, x=0, y=0)
+        self.label3 = ttk.Label(self.frame)
+        self.label3.configure(text='메일주소')
+        self.label3.place(anchor="nw", relx=0.0, rely=0.2, x=0, y=0)
+        self.entry3 = ttk.Entry(self.frame)
+        self.entry3.insert(tk.END,self.mail_address_text)
+        self.entry3.place(anchor="nw", relwidth=0.3, relx=0.11, rely=0.2, x=0, y=0)
 
         # Password part
-        self.password_entry = ttk.Entry(self.mainframe)
-        self.password_entry.insert(tk.END,self.password_text)
-        self.password_entry.place(anchor="nw",relwidth=0.28,relx=0.6,rely=0.21,x=0,y=0)
-        self.password_label = ttk.Label(self.mainframe)
-        self.password_label.configure(text='2차인증PW')
-        self.password_label.place(anchor="nw", relx=0.46, rely=0.21, x=0, y=0)
+        self.label4 = ttk.Label(self.frame)
+        self.label4.configure(text='2차인증PW')
+        self.label4.place(anchor="nw", relx=0.5, rely=0.2, x=0, y=0)
+        self.entry4 = ttk.Entry(self.frame)
+        self.entry4.insert(tk.END,self.password_text)
+        self.entry4.place(anchor="nw", relwidth=0.3, relx=0.65, rely=0.2, x=0, y=0)
+
+    def mail_title_part(self) :
+        # label
+        self.label5 = ttk.Label(self.frame)
+        self.label5.configure(text='메일제목')
+        self.label5.place(anchor="nw", relx=0, rely=0.3, x=0, y=0)
+
+        # entry
+        self.entry5 = ttk.Entry(self.frame)
+        self.entry5.insert(tk.END,self.mail_title_text)
+        self.entry5.place(anchor="nw", relwidth=0.8, relx=0.11, rely=0.3, x=0, y=0)
+        
+        
+    def mail_body_part(self) :
+        # label
+        self.label7 = ttk.Label(self.frame)
+        self.label7.configure(text='메일내용')
+        self.label7.place(anchor="nw", relx=0, rely=0.4, x=0, y=0)
+        # entry
+        self.text1 = tk.Text(self.frame)
+        self.text1.insert(tk.END,self.mail_body_text)
+        self.text1.configure(height=10, width=50)
+        self.text1.place(anchor="nw",relheight=0.5,relwidth=0.8,relx=0.11,rely=0.4,x=0,y=0)
+
+    def analyze_button(self) :
+        button3 = ttk.Button(self.frame)
+        button3.configure(text='파일분류')
+        button3.place(anchor="nw", relx=0.20, rely=0.95, x=0, y=0)
+
+    def send_mail_button(self) :
+        button4 = ttk.Button(self.frame)
+        button4.configure(text='메일전송')
+        button4.place(anchor="nw", relx=0.65, rely=0.95, x=0, y=0)
 
 
-    def submit_part(self) :
-        # 5. submit
-        self.submit_button = ttk.Button(self.mainframe,command=self.run_all)
-        self.submit_button.configure(text='확인')
-        self.submit_button.place(anchor="nw", relx=0.44, rely=0.90, x=0, y=0)
+
 
     def get_contacts_path(self) :
         file_path = askopenfilename(title='contacts.csv 선택')
@@ -136,21 +146,23 @@ class ConfigGUI:
         file_dir= askdirectory(title='PDF 취합폴더 선택')
         self.pdf_dir.set(file_dir)
 
-    def run_all(self) :
+    def analyze(self) :
         self.make_config_file()
-        answer = askyesno(title='Automation',message='정보가 저장되었습니다. 메일을 보내시겠습니까?')
-        if answer :
-            config = ConfigParser()
-            config.read(self.config_path,encoding='utf-8')
-            run(config)
-            showinfo("finished","메일 전송이 완료되었습니다. 전송 실패 내역을 확인하세요.")
-            root.destroy()
-
-        else :
-            root.destroy()
+        config = ConfigParser()
+        config.read(self.config_path,encoding='utf-8')
+        self.wd = analyze_pdf(config)
+        showinfo("Finished",f"파일 분류가 완료되었습니다. 파일위치 : {self.wd}")
+    
+    def send(self) :
+        self.make_config_file()
+        config = ConfigParser()
+        config.read(self.config_path,encoding='utf-8')
+        send_mail(config,self.wd)
+        showinfo("Finished","메일 전송이 완료되었습니다. 전송 실패 내역을 확인하세요.")
         
 
     def make_config_file(self) :
+
         mail_title = self.mail_title_entry.get()
         mail_body = self.mail_body_entry.get("1.0",tk.END)
 
@@ -173,11 +185,8 @@ class ConfigGUI:
     def run(self):
         self.mainwindow.mainloop()
 
-    
-
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("500x300+250+100")
     app = ConfigGUI(root)
     app.run()
